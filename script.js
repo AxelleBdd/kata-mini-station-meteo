4273b08632469063b15bf948cfaf4a66f235df68
AxelleBdd <axbaudinaud@gmail.com>
2025-03-28 11:41:01 +0100

    Fin étape 3

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-/**Je saisis une ville dans le champ texte
-Je clique sur le bouton “OK”
-Je vois alors s’afficher  sur la page les coordonnées GPS de la ville */
-
-const gps = document.querySelector("#gps");
+const gpsData = document.querySelector("#gps");
 const addCityName = document.querySelector("button");
-const affichageCityName = document.querySelector("#city");
+const displayCityName = document.querySelector("#city");
+const displayTemperature = document.querySelector("#temperature");
+const displayDetails = document.querySelector("#details");
 
+function creationCoordinates(elem) {
+    let coordinates = document.createElement("p");
+    let latitude = elem.lat;
+    let longitude = elem.lon;
+    coordinates.innerText = `Coordonnées GPS: ${latitude}, ${longitude}`;
+    fetchWeather(latitude, longitude);
+    gpsData.appendChild(coordinates);
+    displayDetails.innerText = "Température actuelle";
+}
 
-async function getCoordinates(city) {
+async function fetchCoordinates(city) {
     try {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1&limit=1`);
         const coordinatesDetails = await response.json();
 
-        coordinatesDetails.forEach(element => {          
-            let coordonnées = document.createElement("p");
-            coordonnées.innerText = `Coordonnées GPS: ${element.lat}, ${element.lon}` 
-
-            gps.appendChild(coordonnées);
+        coordinatesDetails.forEach(element => {
+            creationCoordinates(element);
         });
     } catch (error) {
-        console.error("Erreur de récupération:", error);
+        console.error("Erreur de récupération des coordonnées:", error);
+    }
+};
+
+async function fetchWeather(latitude, longitude) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,precipitation,relative_humidity_2m`)
+        const weatherDetails = await response.json();
+
+        const detailsCurrent = weatherDetails.current;
+        const temperature = detailsCurrent.temperature_2m;
+
+        displayTemperature.innerText = `${temperature} °C`;
+
+
+    } catch (error) {
+        console.error("Erreur de récupération de la température:", error);
     }
 }
 
 addCityName.addEventListener("click", () => {
     const cityName = document.querySelector("#cityInput").value;
-    
-    affichageCityName.innerText = cityName;    
-    getCoordinates(cityName);
+    displayCityName.innerText = cityName;
+    fetchCoordinates(cityName);
 })
