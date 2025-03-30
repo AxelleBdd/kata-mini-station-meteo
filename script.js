263608da2fbffe1313bf15fbadfa00ca8969dae5
AxelleBdd <axbaudinaud@gmail.com>
2025-03-30 10:18:09 +0200

    Réalisé après la fin du kata, ajout du graphique

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ const precipitationDetails = document.querySelector("#precipitationDetails");
 const displayHumidity = document.querySelector("#humidity");
 const humidityDetails = document.querySelector("#humidityDetails");
 
+
+
 // Déclaration des fonctions
+
+function creationGraph(temperature, time) {
+    let canvas = document.getElementById('myChart'); // Récupération de l'emplacement du HTML
+    let options = {
+        type: "bar", //Choix du type de graph
+      
+        data: {
+            labels: time, // On positionne les données de temps en abscisse
+            datasets: [{
+                label: 'Températures des derniers jours',
+                data: temperature, // On place les données de température
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+          }],
+        },
+      };
+    let myChart = new Chart(canvas, options); // Création du graphique
+}
 
 function creationCoordinates(elem) {
     let coordinates = document.createElement("p"); // Création d'un élément p
@@ -18,6 +38,7 @@ function creationCoordinates(elem) {
     let longitude = elem.lon; // pareil pour la longitude
     coordinates.innerText = `Coordonnées GPS: ${latitude}, ${longitude}`; // On affiche ces données
     fetchWeather(latitude, longitude); // on lance la fonction qui récupère les détails météorologiques
+    fetchForecast(latitude, longitude); // on lance la fonction qui récupère les données des jours précédents
     gpsData.appendChild(coordinates); // On lie coordinates à la div gsp
     displayDetails.innerText = "Température actuelle"; // on affiche le texte indiquant la température de la ville
     precipitationDetails.innerText = "Précipitations actuelles"; // pareil pour les précipitations
@@ -28,7 +49,7 @@ async function fetchCoordinates(city) {
     try {
         const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city}&format=json&addressdetails=1&limit=1`);
         const coordinatesDetails = await response.json(); // Récupération de la réonse de l'API
-        if (coordinatesDetails.length !==0){ // Si le nom de la ville existe, le retour est un tableau non vide
+        if (coordinatesDetails.length !== 0) { // Si le nom de la ville existe, le retour est un tableau non vide
             coordinatesDetails.forEach(element => {
                 creationCoordinates(element); // on lance la fonction de récupération des coordonnées de la ville
                 displayCityName.innerText = city; // on indique le nom de la ville
@@ -37,7 +58,7 @@ async function fetchCoordinates(city) {
             displayCityName.innerText = "Ville non trouvée"; // Indique le problème
             displayDetails.innerText = "Vérifier le nom de la ville"; // Donner une indication à l'utilisateur
         }
-        
+
     } catch (error) {
         console.error("Erreur de récupération des coordonnées:", error); // message d'erreur pour cette API
     }
@@ -51,7 +72,7 @@ async function fetchWeather(latitude, longitude) {
         const detailsCurrent = weatherDetails.current; //Résupération des chemins
         const temperature = detailsCurrent.temperature_2m;
         const precipitation = detailsCurrent.precipitation;
-        const  humidity = detailsCurrent.relative_humidity_2m;
+        const humidity = detailsCurrent.relative_humidity_2m;
 
         displayTemperature.innerText = `${temperature} °C`; // Affichage de la température
 
@@ -63,6 +84,25 @@ async function fetchWeather(latitude, longitude) {
     } catch (error) {
         console.error("Erreur de récupération de la température:", error); // Message d'erreur de la seconde API
     }
+}
+
+async function fetchForecast(latitude, longitude) {
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation&past_days=2`);
+        const forecastData = await response.json();
+
+        let data = forecastData.hourly;
+        let temperatureForecast = data.temperature_2m;
+        let temperatureLastDays = temperatureForecast.slice(0,58); // Récupération des données jusqu'à la date et l'heure du jour
+        let forecastTime = data.time;
+        let timeLastDays = forecastTime.slice(0,58);
+
+        creationGraph(temperatureLastDays, timeLastDays); // Création du graphique
+
+    } catch (error) {
+        console.error("Erreur de récupération des données des jours précédents:", error); // Message d'erreur de la troisième API
+    }
+
 }
 
 addCityName.addEventListener("click", () => {
